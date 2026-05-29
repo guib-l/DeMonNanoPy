18cdd6b7bf294fd1f0a84529d6ee45032803c38f
guib-l <hysfart@gmail.com>
2026-05-29 11:16:31 +0200

    Update tests

--- deMonPy/output.py
+++ deMonPy/output.py
@@ -953,12 +953,13 @@ class read_output(IOread):
 
         if "doforces" in self.flags:
             forces = []
-            filename = os.path.join(self.workdir,"forces.dat")
+            filename = os.path.join(self.workdir,"forces.out")
             with open(filename) as fd:
                 for i,line in enumerate(fd.readlines()):
                     if i==0:continue
                     forces.append( list(map(float,line.split())) )
             print(forces)
+            self.complet_results['forces'] = np.array(forces)
 
         
         self.complet_results['optimization'] = {}
