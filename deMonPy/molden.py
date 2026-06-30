Feat: numerical forces test; Fix: minor fix; Add: contributing, todo-list

deMonPy/molden.py
@@ -104,7 +104,7 @@ def _read_xyz_ext(fileobj, is_charges=False, velocities=False, keep=1, ase_obj=T
         if nbmol % keep == 0:
             images.append(img)
 
-    print(" \u2705 Loaded {} elements from XYZ file.".format(nbmol,))
+    #print(" \u2705 Loaded {} elements from XYZ file.".format(nbmol,))
     return images,np.array(info)
 
 def read_XYZ(filename, **kwargs):

test/test_deMon_rg.py
@@ -157,8 +157,10 @@ class TestDFTB_Rg:
         print(results["energy"]["energy"])
         assert np.allclose(results["energy"]["energy"], -19.51939793, atol=1e-7) 
 
+
+    @pytest.mark.forces
     @pytest.mark.bird
-    def test_argon_derivative(self):
+    def test_argon_grad(self):
 
         parameter_config = deepcopy(parameters)
         parameter_config['DEMON_PARAMETERS']['ACTIVE'].update(